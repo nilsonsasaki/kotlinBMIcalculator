e0a517e4c43fe9348f331eaeffef82fa8237c119
nilsonsasaki <nilsonsasaki@gmail.com>
2021-08-03 17:30:15 -0300

    added function calculate

--- app/src/main/java/com/nilsonsasaki/kotlinbmicalculator/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/kotlinbmicalculator/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.nilsonsasaki.kotlinbmicalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btCalculate: Button = findViewById(R.id.btCalculate)
+        btCalculate.setOnClickListener{(calculate())}
+    }
+    private fun calculate(){
+        val etHeightInput: EditText = findViewById(R.id.etHeightInput)
+        val etWeightInput: EditText = findViewById(R.id.etWeightInput)
+
+
     }
 }
